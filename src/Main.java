6db62a10c56f28ff1488a1827500d9059825bb15
VadimKiselev <mr.vkiselev@gmail.com>
2022-07-01 12:51:14 +0300

    bonus miles

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 2_000;
+        int mile = 20;
+        int bonus = ticket / mile;
+
+        System.out.println("Ваш бонус: "+bonus+" миль");
+    }
+
+}
